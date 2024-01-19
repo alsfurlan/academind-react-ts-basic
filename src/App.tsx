bdb6d308e23ade84427e9fff001e32c3c27fa09a
Anderson Luís Furlan <alsfurlan@gmail.com>
2024-01-19 12:24:26 -0300

    Create the project and first component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import CourseGoal from './components/CourseGoal';
+
+function App() {
+  return (
+    <main>
+      <CourseGoal title='Learn React + TS'>
+        <p>Learn it from the ground up"</p>
+      </CourseGoal>
+    </main>
+  );
+}
+
+export default App;
