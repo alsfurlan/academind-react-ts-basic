1c1121a78f0db8a369d249a89efe270f35ef4492
Anderson Luís Furlan <alsfurlan@gmail.com>
2024-01-26 13:08:45 -0300

    Building a more dynamic & flexible component

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,25 +1,46 @@
-import { type FC } from 'react';
+import { type ReactNode } from 'react';
 import CourseGoal from './CourseGoal';
 import { CourseGoal as CourseGoalType } from '../types/CourseGoal.ts';
+import InfoBox from './InfoBox.tsx';
 
 type CourseGoalListProps = {
   goals: CourseGoalType[];
   onDeleteGoal: (id: number) => void;
+  children: ReactNode;
 };
 
-const CourseGoalList: FC<CourseGoalListProps> = ({ goals, onDeleteGoal }) => {
+const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps) => {
+  if (!goals.length) {
+    return (
+      <InfoBox mode='hint'>
+        You have no course goals yet. Starting add some!
+      </InfoBox>
+    );
+  }
+
+  const warningBox =
+    goals.length >= 4 ? (
+      <InfoBox mode='warning'>
+        {' '}
+        You're collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    ) : null;
+
   return (
-    <ul>
-      {goals.map(({ id, title, description }) => {
-        return (
-          <li key={id}>
-            <CourseGoal id={id} title={title} onDelete={onDeleteGoal}>
-              <p>{description}</p>
-            </CourseGoal>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map(({ id, title, description }) => {
+          return (
+            <li key={id}>
+              <CourseGoal id={id} title={title} onDelete={onDeleteGoal}>
+                <p>{description}</p>
+              </CourseGoal>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
